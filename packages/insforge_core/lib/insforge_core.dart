178442d0d8ee843aef88f081ce96ee3d55f6271f
jwfing <jwfing@gmail.com>
2026-06-08 14:08:33 -0700

    feat(core): add insforge_core package skeleton

---
+++ packages/insforge_core/lib/insforge_core.dart
@@ -0,0 +1,5 @@
+// packages/insforge_core/lib/insforge_core.dart
+/// Core primitives for the InsForge Flutter SDK.
+library insforge_core;
+
+// Exports are added as each component lands in later tasks.
